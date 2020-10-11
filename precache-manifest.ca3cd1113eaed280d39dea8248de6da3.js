e6136a53b7835db26adeb528f14d161967d312ce
jimmy2952 <jimmy2952@gmail.com>
2020-10-11 18:39:02 +0800

    Updates

---
+++ precache-manifest.ca3cd1113eaed280d39dea8248de6da3.js
@@ -0,0 +1,446 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b2d5096f5b1e0b8ec1015639bc877d",
+    "url": "/jimmychenphotography-react/index.html"
+  },
+  {
+    "revision": "a8f27d5bfc2e1151f7ef",
+    "url": "/jimmychenphotography-react/static/css/2.fe7e759a.chunk.css"
+  },
+  {
+    "revision": "d048ce026ec3732d3802",
+    "url": "/jimmychenphotography-react/static/css/main.a785182f.chunk.css"
+  },
+  {
+    "revision": "a8f27d5bfc2e1151f7ef",
+    "url": "/jimmychenphotography-react/static/js/2.e74eb5d9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jimmychenphotography-react/static/js/2.e74eb5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d048ce026ec3732d3802",
+    "url": "/jimmychenphotography-react/static/js/main.4fa425e5.chunk.js"
+  },
+  {
+    "revision": "57047a19275bb516846d",
+    "url": "/jimmychenphotography-react/static/js/runtime-main.0d07db94.js"
+  },
+  {
+    "revision": "0aee388e031b40a23c3d3e9fde1246e9",
+    "url": "/jimmychenphotography-react/static/media/001.0aee388e.jpg"
+  },
+  {
+    "revision": "3fe357dbc8cf1e5944de377102b18727",
+    "url": "/jimmychenphotography-react/static/media/001_compressed.3fe357db.jpg"
+  },
+  {
+    "revision": "6bd91b3686d0f851c99b310a8271416a",
+    "url": "/jimmychenphotography-react/static/media/001_compressed.6bd91b36.jpg"
+  },
+  {
+    "revision": "7ef1a7023981e0b6ae9d581be0818eac",
+    "url": "/jimmychenphotography-react/static/media/001_compressed.7ef1a702.jpg"
+  },
+  {
+    "revision": "826fabf840f40aa09adcd8e30007707f",
+    "url": "/jimmychenphotography-react/static/media/001_compressed.826fabf8.jpg"
+  },
+  {
+    "revision": "8803e95aacc69c1eacb91c27ce61c2aa",
+    "url": "/jimmychenphotography-react/static/media/001_compressed.8803e95a.jpg"
+  },
+  {
+    "revision": "9911f237acd20dbadd535098861ce99a",
+    "url": "/jimmychenphotography-react/static/media/001_compressed.9911f237.jpg"
+  },
+  {
+    "revision": "ba7a2bb4df9e9d8d3bf7aa14d9a6665f",
+    "url": "/jimmychenphotography-react/static/media/001_compressed.ba7a2bb4.jpg"
+  },
+  {
+    "revision": "f2944dca13282069f031b2c0ab6209c8",
+    "url": "/jimmychenphotography-react/static/media/002.f2944dca.jpg"
+  },
+  {
+    "revision": "0add6f0723fc16d8fa81ff53e51d27bf",
+    "url": "/jimmychenphotography-react/static/media/002_compressed.0add6f07.jpg"
+  },
+  {
+    "revision": "4cf8fdc0f02f1f3adb93f88ba70d3388",
+    "url": "/jimmychenphotography-react/static/media/002_compressed.4cf8fdc0.jpg"
+  },
+  {
+    "revision": "5e16de95c3fe25e04a37f2e7206492da",
+    "url": "/jimmychenphotography-react/static/media/002_compressed.5e16de95.jpg"
+  },
+  {
+    "revision": "85bc6393e2c394de4da9c868a7521717",
+    "url": "/jimmychenphotography-react/static/media/002_compressed.85bc6393.jpg"
+  },
+  {
+    "revision": "9099864301951a7c098e084324292f16",
+    "url": "/jimmychenphotography-react/static/media/002_compressed.90998643.jpg"
+  },
+  {
+    "revision": "cbc82edebf1a6bf404c99951ce8a3fbf",
+    "url": "/jimmychenphotography-react/static/media/002_compressed.cbc82ede.jpg"
+  },
+  {
+    "revision": "dfe6a406ff1333fd5498c8f164acf2cf",
+    "url": "/jimmychenphotography-react/static/media/002_compressed.dfe6a406.jpg"
+  },
+  {
+    "revision": "8cf78d6b7ebcd495124d18ee183aa2c0",
+    "url": "/jimmychenphotography-react/static/media/003.8cf78d6b.jpg"
+  },
+  {
+    "revision": "02acd26082203f63565eaca14f4aac02",
+    "url": "/jimmychenphotography-react/static/media/003_compressed.02acd260.jpg"
+  },
+  {
+    "revision": "4b885a893b5904dbf1c50c6ddca25071",
+    "url": "/jimmychenphotography-react/static/media/003_compressed.4b885a89.jpg"
+  },
+  {
+    "revision": "51bb39c3a7c41a7f8d203942581ae422",
+    "url": "/jimmychenphotography-react/static/media/003_compressed.51bb39c3.jpg"
+  },
+  {
+    "revision": "54fd55821b5a7367b0f7ee36ba60e268",
+    "url": "/jimmychenphotography-react/static/media/003_compressed.54fd5582.jpg"
+  },
+  {
+    "revision": "c730defe7064fe01e48818cd1524f9f8",
+    "url": "/jimmychenphotography-react/static/media/003_compressed.c730defe.jpg"
+  },
+  {
+    "revision": "cb2de389ae864f52b62628e6859851a7",
+    "url": "/jimmychenphotography-react/static/media/003_compressed.cb2de389.jpg"
+  },
+  {
+    "revision": "d7a067d9001101f11d8d0560f4ebb91a",
+    "url": "/jimmychenphotography-react/static/media/003_compressed.d7a067d9.jpg"
+  },
+  {
+    "revision": "2ca7ff308b80c816f146ce69289d2e3b",
+    "url": "/jimmychenphotography-react/static/media/004.2ca7ff30.jpg"
+  },
+  {
+    "revision": "162643b2cce00692b431915d870d8584",
+    "url": "/jimmychenphotography-react/static/media/004_compressed.162643b2.jpg"
+  },
+  {
+    "revision": "534ea6be8bd0e579594ac50fe1a5b3cb",
+    "url": "/jimmychenphotography-react/static/media/004_compressed.534ea6be.jpg"
+  },
+  {
+    "revision": "8e0e0ca7ef3274f601ec8948c3071b94",
+    "url": "/jimmychenphotography-react/static/media/004_compressed.8e0e0ca7.jpg"
+  },
+  {
+    "revision": "a2d90cfbc433fee0e1cd718bc60b4c2b",
+    "url": "/jimmychenphotography-react/static/media/004_compressed.a2d90cfb.jpg"
+  },
+  {
+    "revision": "ab17a8c25af325109f811bc443cef95b",
+    "url": "/jimmychenphotography-react/static/media/004_compressed.ab17a8c2.jpg"
+  },
+  {
+    "revision": "f8649327906472238de97d985cb0e337",
+    "url": "/jimmychenphotography-react/static/media/004_compressed.f8649327.jpg"
+  },
+  {
+    "revision": "fc0ed755022b29549e2400673313d15a",
+    "url": "/jimmychenphotography-react/static/media/004_compressed.fc0ed755.jpg"
+  },
+  {
+    "revision": "d933f1864551a257b10c90d033d0014a",
+    "url": "/jimmychenphotography-react/static/media/005.d933f186.jpg"
+  },
+  {
+    "revision": "496b6be318cc280c63a7af3e4288a893",
+    "url": "/jimmychenphotography-react/static/media/005_compressed.496b6be3.jpg"
+  },
+  {
+    "revision": "83306a26d8f6bce2550a9c4027ae0a26",
+    "url": "/jimmychenphotography-react/static/media/005_compressed.83306a26.jpg"
+  },
+  {
+    "revision": "8e227f29cad2573995346d92d23fc323",
+    "url": "/jimmychenphotography-react/static/media/005_compressed.8e227f29.jpg"
+  },
+  {
+    "revision": "a11ff6f1e5997cb1b35be16e971d76d0",
+    "url": "/jimmychenphotography-react/static/media/005_compressed.a11ff6f1.jpg"
+  },
+  {
+    "revision": "b0572e140f7755c3fd4300ccd7441d1a",
+    "url": "/jimmychenphotography-react/static/media/005_compressed.b0572e14.jpg"
+  },
+  {
+    "revision": "c40950e4a44e4076e1e09d685ee71c4f",
+    "url": "/jimmychenphotography-react/static/media/005_compressed.c40950e4.jpg"
+  },
+  {
+    "revision": "ed9ad7f0ae0d59cfe9397d38eb5acf49",
+    "url": "/jimmychenphotography-react/static/media/005_compressed.ed9ad7f0.jpg"
+  },
+  {
+    "revision": "0c9c190ee1cdff216f1dcba286eb93b3",
+    "url": "/jimmychenphotography-react/static/media/006.0c9c190e.jpg"
+  },
+  {
+    "revision": "0aae57ba2d1fd73fe7d3115d33b22cd5",
+    "url": "/jimmychenphotography-react/static/media/006_compressed.0aae57ba.jpg"
+  },
+  {
+    "revision": "a10b3d60cc0734bf70b54f9dd4f8dfe7",
+    "url": "/jimmychenphotography-react/static/media/006_compressed.a10b3d60.jpg"
+  },
+  {
+    "revision": "b47579ad1fe9d9777c2beeac2cc8d603",
+    "url": "/jimmychenphotography-react/static/media/006_compressed.b47579ad.jpg"
+  },
+  {
+    "revision": "b80a81d7ac9934e93faa75ee673c023b",
+    "url": "/jimmychenphotography-react/static/media/006_compressed.b80a81d7.jpg"
+  },
+  {
+    "revision": "c032f16fb180d4a8fcabf7f625c6ec5f",
+    "url": "/jimmychenphotography-react/static/media/006_compressed.c032f16f.jpg"
+  },
+  {
+    "revision": "dc92faae41783312869824ed9cefc414",
+    "url": "/jimmychenphotography-react/static/media/006_compressed.dc92faae.jpg"
+  },
+  {
+    "revision": "ea6bdd6724d5abeed16023d46874ec95",
+    "url": "/jimmychenphotography-react/static/media/006_compressed.ea6bdd67.jpg"
+  },
+  {
+    "revision": "4e0c5f79fbf7c9c5ee010b4810352b8f",
+    "url": "/jimmychenphotography-react/static/media/007.4e0c5f79.jpg"
+  },
+  {
+    "revision": "00ab6a8c3f30cee1b1240f4f3845847c",
+    "url": "/jimmychenphotography-react/static/media/007_compressed.00ab6a8c.jpg"
+  },
+  {
+    "revision": "3a82f8b2726d17e8ac3cbf95330957a6",
+    "url": "/jimmychenphotography-react/static/media/007_compressed.3a82f8b2.jpg"
+  },
+  {
+    "revision": "3c03dd6da058ca4d37d2d24cee523d81",
+    "url": "/jimmychenphotography-react/static/media/007_compressed.3c03dd6d.jpg"
+  },
+  {
+    "revision": "552ede8975acaaf394c32168d7852724",
+    "url": "/jimmychenphotography-react/static/media/007_compressed.552ede89.jpg"
+  },
+  {
+    "revision": "9850dc7b523ef7534affbdcf26de4ad2",
+    "url": "/jimmychenphotography-react/static/media/007_compressed.9850dc7b.jpg"
+  },
+  {
+    "revision": "a3d7fb9ed5ceace62eb856db2ad07411",
+    "url": "/jimmychenphotography-react/static/media/007_compressed.a3d7fb9e.jpg"
+  },
+  {
+    "revision": "fba30f002cd82bd8a0299825ad69542f",
+    "url": "/jimmychenphotography-react/static/media/007_compressed.fba30f00.jpg"
+  },
+  {
+    "revision": "259389f3ab1736558abf6867ba69183f",
+    "url": "/jimmychenphotography-react/static/media/008_compressed.259389f3.jpg"
+  },
+  {
+    "revision": "c270027c298e8fe9b8f5a36d95cf562d",
+    "url": "/jimmychenphotography-react/static/media/008_compressed.c270027c.jpg"
+  },
+  {
+    "revision": "d724eabb146e047fe9f3c7e6ab22125a",
+    "url": "/jimmychenphotography-react/static/media/008_compressed.d724eabb.jpg"
+  },
+  {
+    "revision": "f64c14e1e15edad10b311f65e2756143",
+    "url": "/jimmychenphotography-react/static/media/008_compressed.f64c14e1.jpg"
+  },
+  {
+    "revision": "22113084157f1338132c6eae2258f41c",
+    "url": "/jimmychenphotography-react/static/media/009_compressed.22113084.jpg"
+  },
+  {
+    "revision": "3879f730a6888cc3bb99ca6bb245dcdf",
+    "url": "/jimmychenphotography-react/static/media/009_compressed.3879f730.jpg"
+  },
+  {
+    "revision": "78bd356e80394ea7648386f40b4c0981",
+    "url": "/jimmychenphotography-react/static/media/009_compressed.78bd356e.jpg"
+  },
+  {
+    "revision": "f6093829d7307a14ad604ddc3d6a120d",
+    "url": "/jimmychenphotography-react/static/media/009_compressed.f6093829.jpg"
+  },
+  {
+    "revision": "7510c5f61f68afc7565997cef0294b3d",
+    "url": "/jimmychenphotography-react/static/media/010_compressed.7510c5f6.jpg"
+  },
+  {
+    "revision": "883f3418b554bbcd068d85a953150807",
+    "url": "/jimmychenphotography-react/static/media/010_compressed.883f3418.jpg"
+  },
+  {
+    "revision": "9288116fc567b19a02bd9f74bf236cac",
+    "url": "/jimmychenphotography-react/static/media/010_compressed.9288116f.jpg"
+  },
+  {
+    "revision": "fb488b0d4e282cd3469bb9e83520e891",
+    "url": "/jimmychenphotography-react/static/media/010_compressed.fb488b0d.jpg"
+  },
+  {
+    "revision": "109c1037d03d8fa547ab38c60bc1e053",
+    "url": "/jimmychenphotography-react/static/media/011_compressed.109c1037.jpg"
+  },
+  {
+    "revision": "8b1a2b00e75fbcb43dd31fdca2cf2e3e",
+    "url": "/jimmychenphotography-react/static/media/011_compressed.8b1a2b00.jpg"
+  },
+  {
+    "revision": "d4e12eea23f8f89d052537a4bfe0f165",
+    "url": "/jimmychenphotography-react/static/media/011_compressed.d4e12eea.jpg"
+  },
+  {
+    "revision": "dc7ffe04a0f70f4ff8f97f90c8499da6",
+    "url": "/jimmychenphotography-react/static/media/011_compressed.dc7ffe04.jpg"
+  },
+  {
+    "revision": "538106f4affbde6225383fb6594c635e",
+    "url": "/jimmychenphotography-react/static/media/012_compressed.538106f4.jpg"
+  },
+  {
+    "revision": "827642f519582f3853152006ba25069a",
+    "url": "/jimmychenphotography-react/static/media/012_compressed.827642f5.jpg"
+  },
+  {
+    "revision": "b4ff3f1cc98e2e64c7092fef0d0cd4ca",
+    "url": "/jimmychenphotography-react/static/media/012_compressed.b4ff3f1c.jpg"
+  },
+  {
+    "revision": "ef43e0933613e1128aaecbfa1bf63821",
+    "url": "/jimmychenphotography-react/static/media/012_compressed.ef43e093.jpg"
+  },
+  {
+    "revision": "86dc23121870ee2835e8dd15b8230730",
+    "url": "/jimmychenphotography-react/static/media/013_compressed.86dc2312.jpg"
+  },
+  {
+    "revision": "983b53418f18f8cf1fbd16955a9739e6",
+    "url": "/jimmychenphotography-react/static/media/013_compressed.983b5341.jpg"
+  },
+  {
+    "revision": "cec86d4c39d12ab0e0d5878c8e11df69",
+    "url": "/jimmychenphotography-react/static/media/013_compressed.cec86d4c.jpg"
+  },
+  {
+    "revision": "f4242a99e4dbd536a69f566a8d3905cb",
+    "url": "/jimmychenphotography-react/static/media/013_compressed.f4242a99.jpg"
+  },
+  {
+    "revision": "11a53c376e0de35d276d6ba7233cb8ec",
+    "url": "/jimmychenphotography-react/static/media/014_compressed.11a53c37.jpg"
+  },
+  {
+    "revision": "144aef966c5b55280af8c26336f409df",
+    "url": "/jimmychenphotography-react/static/media/014_compressed.144aef96.jpg"
+  },
+  {
+    "revision": "608d065bec8bad4e1dd2a8197553bad7",
+    "url": "/jimmychenphotography-react/static/media/014_compressed.608d065b.jpg"
+  },
+  {
+    "revision": "ad2d1da71965547d6f21c8602015eed8",
+    "url": "/jimmychenphotography-react/static/media/014_compressed.ad2d1da7.jpg"
+  },
+  {
+    "revision": "ec6965f5e1882bd04f8084b3381f1a3d",
+    "url": "/jimmychenphotography-react/static/media/Savoye_LET_plain_gray.ec6965f5.png"
+  },
+  {
+    "revision": "b5d2d9b811924a6a5ae1df1eb7d24d93",
+    "url": "/jimmychenphotography-react/static/media/about.b5d2d9b8.jpg"
+  },
+  {
+    "revision": "ee83cee65dcfaf6e0d29eadc9070f223",
+    "url": "/jimmychenphotography-react/static/media/chiayicity.ee83cee6.jpg"
+  },
+  {
+    "revision": "1d43d103c719cf36ec5df3bee4957a4b",
+    "url": "/jimmychenphotography-react/static/media/facebook.1d43d103.svg"
+  },
+  {
+    "revision": "8453eb57f537c5c59856670f721b14f1",
+    "url": "/jimmychenphotography-react/static/media/hsinchucity.8453eb57.jpg"
+  },
+  {
+    "revision": "dc3f4116853ec9a4d69f2f243d1f5bc2",
+    "url": "/jimmychenphotography-react/static/media/hsinchucounty.dc3f4116.jpg"
+  },
+  {
+    "revision": "454a611f128df6b1de9bd6dc8ea9481c",
+    "url": "/jimmychenphotography-react/static/media/hualiencounty.454a611f.jpg"
+  },
+  {
+    "revision": "5f1d3aa976e7a06f46c46437e0748c99",
+    "url": "/jimmychenphotography-react/static/media/instagram.5f1d3aa9.svg"
+  },
+  {
+    "revision": "787a7e2bd2cf44908785ac89cb2baba5",
+    "url": "/jimmychenphotography-react/static/media/kaohsiungcity.787a7e2b.jpg"
+  },
+  {
+    "revision": "94fa1a32d8c8a0243862b41059249be0",
+    "url": "/jimmychenphotography-react/static/media/keelungcity.94fa1a32.jpg"
+  },
+  {
+    "revision": "1fbfd4bcffccb94e8e8a5ea70616b296",
+    "url": "/jimmychenphotography-react/static/media/lg.1fbfd4bc.woff"
+  },
+  {
+    "revision": "2ec2cb2199d4d881e6a6ad86690f6add",
+    "url": "/jimmychenphotography-react/static/media/lg.2ec2cb21.svg"
+  },
+  {
+    "revision": "f4292655f93dd12d9b8e4fc067ef2489",
+    "url": "/jimmychenphotography-react/static/media/lg.f4292655.ttf"
+  },
+  {
+    "revision": "bf316fa5ed5863b4b956b07af3fb6d85",
+    "url": "/jimmychenphotography-react/static/media/miaolicounty.bf316fa5.jpg"
+  },
+  {
+    "revision": "2b803b4a4996972d00f25114a7ef7a1b",
+    "url": "/jimmychenphotography-react/static/media/newtaipeicity.2b803b4a.jpg"
+  },
+  {
+    "revision": "b50862393235c8d20f0b7840a21340fc",
+    "url": "/jimmychenphotography-react/static/media/taichungcity.b5086239.jpg"
+  },
+  {
+    "revision": "3778741bdb51fdccee11cf4a31a8eb84",
+    "url": "/jimmychenphotography-react/static/media/tainancity.3778741b.jpg"
+  },
+  {
+    "revision": "db1faf3e6c93a6fc42fa304a959862ee",
+    "url": "/jimmychenphotography-react/static/media/taipeicity.db1faf3e.jpg"
+  },
+  {
+    "revision": "64458cca93ebc34265b296753b5e4e8a",
+    "url": "/jimmychenphotography-react/static/media/taitungcounty.64458cca.jpg"
+  },
+  {
+    "revision": "819869c719b31487739b2d70d6c4ca1a",
+    "url": "/jimmychenphotography-react/static/media/yilancounty.819869c7.jpg"
+  }
+]);
